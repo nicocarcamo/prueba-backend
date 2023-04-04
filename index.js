44d0c89373c8259b3ced3625b6f20e9cebdec36e
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2023-04-04 23:02:17 +0000

    endpoint post agregado

--- index.js
+++ index.js
@@ -6,44 +6,59 @@ const port = 3000;
 
 // filtramos según parámetro de consulta
 const filterItineraries = (origin, destination, eta, transitTime) => {
-  return result => (
-    (origin === undefined || result.pol.name.toLowerCase() === origin.toLowerCase()) &&
-    (destination === undefined || result.pod.name.toLowerCase() === destination.toLowerCase()) &&
-    (eta === undefined || result.eta.includes(eta)) &&
-    (transitTime === undefined || result.transit_time.toString() === transitTime)
-  );
+    return result => (
+        (origin === undefined || result.pol.name.toLowerCase() === origin.toLowerCase()) &&
+        (destination === undefined || result.pod.name.toLowerCase() === destination.toLowerCase()) &&
+        (eta === undefined || result.eta.includes(eta)) &&
+        (transitTime === undefined || result.transit_time.toString() === transitTime)
+    );
 };
 
-// definimos la ruta que maneja las solicitudes
+// definimos la ruta que maneja las solicitudes GET
 app.get('/itineraries', (req, res) => {
-  const origin = req.query.origin;
-  const destination = req.query.destination;
-  const eta = req.query.eta;
-  const transitTime = req.query.transitTime;
+    const origin = req.query.origin;
+    const destination = req.query.destination;
+    const eta = req.query.eta;
+    const transitTime = req.query.transitTime;
 
-  fs.readFile('./testDataset.json', (err, data) => {
-    //error catch
-    if (err) {
-      console.log(err);
-      res.status(500).send('Error interno del servidor');
-    } else {
-      // else parsear json  
-      const jsonData = JSON.parse(data);
-      const itineraries = jsonData.results.filter(filterItineraries(origin, destination, eta, transitTime)).map(result => {
-        return {
-          Origen: result.pol.name,
-          Destino: result.pod.name,
-          Naviera: result.carrier.short_name,
-          ETD: result.etd,
-          ETA: result.eta
+    fs.readFile('./testDataset.json', (err, data) => {
+        //error catch
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error interno del servidor');
+        } else {
+            // else parsear json  
+            const jsonData = JSON.parse(data);
+            const itineraries = jsonData.results.filter(filterItineraries(origin, destination, eta, transitTime)).map(result => {
+                return {
+                    Origen: result.pol.name,
+                    Destino: result.pod.name,
+                    Naviera: result.carrier.short_name,
+                    ETD: result.etd,
+                    ETA: result.eta
+                }
+            });
+            // enviamos respuesta en el formato solicitado
+            res.send(itineraries);
         }
-      });
-      // enviamos respuesta en el formato solicitado
-      res.send(itineraries);
-    }
-  });
+    });
+});
+
+// definimos la ruta que maneja las solicitudes POST para agregar itinerarios
+app.post('/itineraries', (req, res) => {
+    // obtenemos los datos enviados en la solicitud POST
+    const newItinerary = req.body;
+
+    // agregamos el nuevo itinerario a una lista temporal en memoria
+    const tempData = fs.readFileSync('./testDataset.json');
+    const jsonData = JSON.parse(tempData);
+    jsonData.results.push(newItinerary);
+
+    // enviamos una respuesta exitosa
+    res.status(201).send('Itinerario agregado con éxito');
+
 });
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
+    console.log(`Servidor escuchando en el puerto ${port}`);
 });
